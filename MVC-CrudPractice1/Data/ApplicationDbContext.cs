6ecf58319a1cb91b01b4e03327eeb7339585837f
janthony25 <junanthony.business@gmail.com>
2024-02-17 14:04:10 +1300

    Add DbContext

---
+++ MVC-CrudPractice1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_CrudPractice1.Models;
+
+namespace MVC_CrudPractice1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
